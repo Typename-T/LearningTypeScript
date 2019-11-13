e227278133b9b5e1b50fd75dad59553687a6a4f0
Ilya <Ilya@gmail.com>
2019-11-13 21:51:05 +0500

    Проект JavaScript

---
+++ test/app.js
@@ -0,0 +1,33 @@
+let persone = {
+  name: "John",
+  age: 25,
+  isMarrid: false
+}
+console.log(persone.name);
+
+let arr = ['plum.png','orange.jpg','aplle.bmp'];
+
+console.log(arr[1]);
+
+//alert('Hello world');
+//let answer = confirm('are you here?');
+//console.log(answer);
+
+/*let answer = prompt('are you here?', "Да");
+console.log(typeof(arr));*/
+
+let incr = 10;
+    decr = 10;
+
+
+
+console.log(decr);
+console.log(incr);
+
+console.log(5%2);
+
+console.log("2" == 2);
+
+let isChecked = true;
+    isClose = true;
+    console.log(isChecked && isChecked);
